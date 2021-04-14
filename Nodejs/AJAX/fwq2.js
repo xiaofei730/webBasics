32c40d93b33f8c2cb85347b9df9fd81b2fda0eb6
linfei <337262953@qq.com>
2021-04-14 22:25:54 +0800

    新增：跨域问题JSONP方法

--- Nodejs/AJAX/fwq2.js
+++ Nodejs/AJAX/fwq2.js
@@ -13,13 +13,22 @@ server.listen('9000', () => {
 server.on('request', (req, resp) => {
     var baseURL = 'http://' + req.headers.host + '/';
     const myURL = new URL(req.url, baseURL);
+    // console.log(req.url);
+    // console.log(myURL.pathname);
     if (req.method == 'GET') {
         if (myURL.pathname == '/') {
             
-        } else if (myURL.pathname == 'gets'){
+        } else if (myURL.pathname == '/gets'){
             resp.end('999');
-        } else {
-            //myURL.pathname 或者req.url
+        } else if (myURL.pathname  == '/kuayus'){
+            var fun_name =  myURL.searchParams.get('callback');
+            console.log(fun_name);
+            var json_str = '[{"id": "2", "name": "lisi"}]';
+            // resp.end('alert("90")');
+            // resp.end('console.log('+json_str+')');
+            resp.end(fun_name + '('+json_str+')');
+        }else {
+            //myURL.pathname
             fs.readFile('.' + myURL.pathname, (err, data_str) => {
                 if(!err) {
                     resp.end(data_str);
@@ -29,7 +38,7 @@ server.on('request', (req, resp) => {
                 }
             });
         }
-    } else {
+    } else if(req.method == 'POST') {
         
     }
 });
