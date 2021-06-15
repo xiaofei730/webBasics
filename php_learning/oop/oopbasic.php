cc967c52c3dcaa2d9bf7d33691ca4c06efa95989
linfei <linfei@123.com>
2021-06-15 18:01:19 +0800

    新增：抽象类和接口学习

--- php_learning/oop/oopbasic.php
+++ php_learning/oop/oopbasic.php
@@ -346,5 +346,220 @@
  * 2、如果一个方法不允许被其他类重写，可以用final修饰
  * 
  * 
+ * （3）abstract 【抽象的】
+ * 1、abstract修饰的方法是抽象方法，修饰的类是抽象类
+ * 2、只有方法的声明没有方法的实现称为抽象方法
+ * 3、一个类中至于有一个方法是抽象方法，这个类必须是抽象类
+ * 4、抽象类的特点是不能被实例化
+ * 5、子类继承类抽象类，就必须重新实现父类的所有的抽象方法，否则不允许实例化
+ * 6、类中没有抽象方法也可以声明成抽象类，用来阻止类的实例化
+ * 
+ * abstract class Person {
+ *      public abstract function setInfo();
+ *      public function getInfo() {
+ *          echo '获取信息','<br>';
+ *      }
+ * }
+ * 
+ * class Student extends Person {
+ *      public function setInfo() {
+ *          echo '重新实现父类的抽象方法<br>';
+ *      }
+ * }
+ * 
+ * $stu = new Student;
+ * $stu->setInfo();
+ * 
+ * 抽象类的作用：
+ * 1、定义命名规范
+ * 
+ * 2、阻止实例化，如果一个类中所有的方法都是静态方法，这时候没有必要区实例化，可以通过abstract来阻止实例化
+ * 
  * 
  */
+
+
+/**
+ * 类常量
+ * 1、类常量是const常量
+ * class Student {
+ *  const ADD = '地址不详';
+ * }
+ * 
+ * echo Student::ADD;
+ * 
+ * 问题：define常量和const常量的区别？
+ * 答：const常量可以做类成员，define常量不可以做类成员
+ * 
+ * 问题：常量和静态属性的区别？
+ * 答：相同点：都在加载类的时候分配空间
+ *    不相同点：常量的值不可以更改，静态属性的值可以更改 
+ * 
+ * 
+ */
+
+
+/**
+ * 接口（interface）
+ * 
+ * 1、接口
+ * （1）如果一个类中所有的方法都是抽象方法，那么这个抽象类可以声明成接口
+ * （2）接口是一个特殊的抽象类，接口中只能有抽象方法和常量
+ * （3）接口中的抽象方法只能是public，可以省略，默认也是public的
+ * （4）通过implements关键字来实现接口
+ * （5）不能使用abstract和final来修饰接口中的抽象方法
+ * 
+ * 声明接口
+ * interface IPerson {
+ *  function func1();
+ *  function func2();
+ * }
+ * 
+ * class Student implements IPerson {
+ *  public function fun1() {
+ * 
+ *  }
+ *  public function fun2() {
+ * 
+ *  }
+ * }
+ * 
+ * 
+ * 2、接口的多重实现
+ * 类不允许多重继承，但是接口允许多重实现
+ * 
+ * interface IPic1{
+ *  function fun1();
+ * }
+ * interface IPic2{
+ *  function fun2();
+ * }
+ * 
+ * //接口允许多重实现
+ * class Student implements IPic1,IPic2 {
+ *  public function fun1() {
+ * 
+ *  }
+ *  public function fun2() {
+ * 
+ *  }
+ * }
+ * 
+ * 注意：在接口的多重实现中，如果有同名的方法，只要实现一次即可
+ * 
+ */
+
+
+/**
+ * 匿名类
+ * <?php 
+ * $stu = new class {
+ *  public $name = 'tom';
+ *  public function __construct() {
+ *      echo '构造函数<br>';
+ *  }
+ * 
+ * }
+ * echo $stu->name;
+ * 
+ * 好处：
+ * （1）如果类只被实例化一次就可以使用匿名类
+ * （2）好处，实例化完毕后就回收类类的空间
+ * 
+ * 
+ */
+
+
+/**
+ * 方法绑定（PHP7.0支持）
+ * 
+ * 将方法绑定到对象上
+ * 语法：
+ * 闭包->call(对象)
+ * 
+ * 
+ * $lang = 'en';
+ * class Student {
+ * 
+ * }
+ * 
+ * if($lang == 'ch') {
+ *  $fun = function() {
+ *      echo '我是一名学生';
+ *  }
+ * } else {
+ *  $fun = function() {
+ *      echo 'i am a student';
+ *  }
+ * }
+ * $stu = new Student;
+ * 
+ * $fun->call($stu);
+ * 
+ * 
+ * 
+ */
+
+/**
+ * 
+ * 异常处理
+ * 集中处理在代码块中发生的异常。
+ * 在代码块中发生了异常直接抛出，代码块中不处理异常，将异常集中起来一起处理
+ * 
+ * 1、使用的关键字
+ * try:监测代码块
+ * catch:捕获异常
+ * throw:抛出异常
+ * finally:无论有无异常都会执行，可以省略
+ * Exception:异常类
+ * 
+ * 语法结构
+ * try {
+ * 
+ * }catch(Exception $ex) {
+ * 
+ * } finally{
+ * 
+ * }
+ * 
+ * 
+ * 2、自定义异常
+ * 场景：如何实现异常的分类处理？比如 异常有三个级别异常对应三种处理方式
+ * 自定义三种异常即可
+ * 所有异常类的父类是Exception,Exception中的方法不允许重写
+ * 
+ * 
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
